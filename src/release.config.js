742fdfba5880b0ee9e7d59e490cda95efe4ee251
tomasz kowalczyk <tomasz.kowalczyk1983@gmail.com>
2025-09-27 17:25:19 +0100

    chore: added changelog file and updated release config

---
+++ src/release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    branches: [
+        { name: "main" }
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer', // Analyze commits to determine version
+        '@semantic-release/release-notes-generator', // Generate release notes
+        '@semantic-release/changelog', // Update CHANGELOG.md
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'composer.json'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+};
